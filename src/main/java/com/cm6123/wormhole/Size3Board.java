31be2095bbba4867836d315e2a9b597f28ba20f3
Fei Liu <LiuF29@cardiff.ac.uk>
2022-03-06 16:06:25 +0000

    fixed errors of enter player's name not looping

--- src/main/java/com/cm6123/wormhole/Size3Board.java
+++ src/main/java/com/cm6123/wormhole/Size3Board.java
@@ -6,28 +6,29 @@ import java.util.List;
 public class Size3Board {
 
     private final int size3Board[][];
-    private final HashMap< Players,Integer>PlayersPosition = new HashMap<Players, Integer>(6);
+    private final int rows = 3;
+    private final int columns = 3;
+    private final HashMap<Players, Integer> PlayersPosition = new HashMap<Players, Integer>(6);
 
-    public Size3Board(List<Players> players){
-//    public Boards(){
+    public Size3Board(List<Players> players) {
         size3Board = new int[3][3];
-        for(int i=0; i< size3Board.length; i++){
-            for(int j=0; j< size3Board.length; j++){
-                size3Board[i][j] = i+j;
+        for (int i = 0; i < size3Board.length; i++) {
+            for (int j = 0; j < size3Board.length; j++) {
+                size3Board[i][j] = i + j;
             }
         }
 
-//        for(Players player : players){
-//            PlayersPosition.put(player,1);
-//
-//        }
     }
 
-    public boolean movePlayerPosition(Players player, int position){
-        int CurrentPosition = PlayersPosition.get(player);
-        CurrentPosition += position;
-        PlayersPosition.put(player, position);
-        return false;
-    }
+    public String create() {
+        StringBuilder sb = new StringBuilder();
+        boolean rightToLeft = true;
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = 0; j < columns; j++) {
+                sb.append(size3Board[rows][columns] + "\t");
 
+            }
+        }
+        return sb.toString();
+    }
 }
--- src/main/java/com/cm6123/wormhole/WormholeGame.java
+++ src/main/java/com/cm6123/wormhole/WormholeGame.java
@@ -12,14 +12,11 @@ public class WormholeGame {
 
         System.out.println("Please enter the width dimension that you want to play the game (5-10)");
         int BoardSize = sc.nextInt();
-        boolean BoardSizeExceedMaxMin = false;
 
         ArrayList<Integer> boards = new ArrayList<>();
-        if(BoardSizeExceedMaxMin){
-            if(BoardSize >= 5 || BoardSize <=10){
-                System.out.println("Thank you! Your board size is"+ BoardSize + ".");
-                boards.add(BoardSize);
-            }
+        if(BoardSize >= 5 && BoardSize <=10){
+            System.out.println("Thank you! Your board size is"+ BoardSize + ".");
+            boards.add(BoardSize);
         }
         else if (BoardSize < 5) {
             System.out.println("Sorry, the minimum size of the board is 5.");
@@ -30,19 +27,16 @@ public class WormholeGame {
 
         System.out.println("Please enter the number of player (2-6)");
         int NoOfPlayer = sc.nextInt();
-        boolean PlayersExceedMaxMin = false;
-
         ArrayList<Players> players = new ArrayList<>();
-        if(PlayersExceedMaxMin){
-            if (NoOfPlayer >= 2 || NoOfPlayer <= 6) {
-                for (int i = 0; i < NoOfPlayer; i++) {
+        if (NoOfPlayer >= 2 && NoOfPlayer <= 6) {
+            for (int i = 0; i < NoOfPlayer; i++) {
                     System.out.println("Please enter player name");
                     String name = sc.next();
                     Players playerName = new Players(name);
                     players.add(playerName);
-                }
             }
         }
+
         else if (NoOfPlayer < 2) {
             System.out.println("Sorry, the minimum players for this game is 2 players.");
         }
